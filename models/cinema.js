5f7501466a2281cda1287c6fd71bf21af10fb012
Neddon13 <91260623+Neddon13@users.noreply.github.com>
2021-11-24 14:47:23 +0000

    12 passing

---
+++ models/cinema.js
@@ -0,0 +1,31 @@
+const Cinema = function (films) {
+  this.films = films;
+};
+
+Cinema.prototype.listOfTitles = function () {
+  return this.films.map(film => film.title)
+};
+
+Cinema.prototype.findByTitle = function (title) {
+  return this.films.find(film => title === film.title);
+}
+
+Cinema.prototype.filterFilms = function (genre) {
+  return this.films.filter(film => genre === film.genre);
+}
+
+Cinema.prototype.filmsByYear = function (year) {
+  return this.films.some(film => year === film.year);
+}
+
+Cinema.prototype.findByLength = function (length) {
+  return this.films.every(film => length > film.length);
+}
+
+Cinema.prototype.totalRunningTime = function () {
+  return this.films.reduce((acc, currentFilm) => acc + currentFilm.length, 0);
+}
+
+
+ 
+module.exports = Cinema;
